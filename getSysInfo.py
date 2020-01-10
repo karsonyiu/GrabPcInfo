18f4c92b1e5c9d4d7a0e239e0cfb2efafebce3f5
kianyiu <kianyiu20@gmail.com>
2020-01-10 10:06:36 +0000

    Feature_0002 Use python to grab computer information

---
+++ getSysInfo.py
@@ -0,0 +1,8 @@
+#-----------------------------------------------------------------------------
+# This script is used to retrieve a computer hardware/windows OS information
+#-----------------------------------------------------------------------------
+#
+# TODO:: Use python to retrieve computer information
+#
+
+
