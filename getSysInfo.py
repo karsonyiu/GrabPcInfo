3e4ca0d525d4f28ac4e5205055145ee1ce09cfd4
kianyiu <kianyiu20@gmail.com>
2020-01-12 16:11:23 +0000

    Grab computer information using Win32_ComputerSystem()

--- getSysInfo.py
+++ getSysInfo.py
@@ -4,5 +4,35 @@
 #
 # TODO:: Use python to retrieve computer information
 #
+#
+# pip install wmi
+#
+
+import wmi
 
 
+
+try:
+    conn = wmi.WMI()                                                               # Local computer
+    #conn = wmi.WMI("192.168.1.10", user="administrator", password="admin_password")
+ 
+    print('\n---------- Win32_ComputerSystem -------------')
+    for cs in conn.Win32_ComputerSystem():
+        print('Manufacturer: {}'.format(cs.Manufacturer))
+        print('Model: {}'.format(cs.Model))
+        print('SystemSKUNumber: {}'.format(cs.SystemSKUNumber))
+        print('Product Name: {}'.format(cs.SystemFamily))
+        print('Caption: {}'.format(cs.Caption))
+        print('Computer Name: {}'.format(cs.Name))
+        print('DNSHostName: {}'.format(cs.DNSHostName))
+        print('Domain: {}'.format(cs.Domain))
+        print('Workgroup: {}'.format(cs.Workgroup))
+        print('UserName: {}'.format(cs.UserName))
+        print('PrimaryOwnerName: {}'.format(cs.PrimaryOwnerName))
+ 
+except Exception as e:
+    print(e)
+
+
+
+ 
